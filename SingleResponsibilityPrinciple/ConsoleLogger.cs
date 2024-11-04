8fef4fdc6c08c0f6822ae698ff79c8db70e02b32
Michael Ludwig <mludwig2@css.edu>
2024-11-03 19:39:30 -0600

    Request 405 Complete

--- SingleResponsibilityPrinciple/ConsoleLogger.cs
+++ SingleResponsibilityPrinciple/ConsoleLogger.cs
@@ -9,11 +9,20 @@ namespace SingleResponsibilityPrinciple
         public void LogWarning(string message, params object[] args)
         {
             Console.WriteLine(string.Concat("WARN: ", message), args);
+            using (StreamWriter logfile = File.AppendText("log.xml"))
+            {
+                logfile.WriteLine("<log><type>WARN</type><message>" + message + "</message></log> ", args);
+            }
         }
 
         public void LogInfo(string message, params object[] args)
         {
             Console.WriteLine(string.Concat("INFO: ", message), args);
+            using (StreamWriter logfile = File.AppendText("log.xml"))
+            {
+                logfile.WriteLine("<log><type>INFO</type><message>" + message + "</message></log> ", args);
+            }
         }
+
     }
 }
